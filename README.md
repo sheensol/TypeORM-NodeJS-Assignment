0070230a552e7fd380bb7eadcb341d83bc439312
Imran Haider <sheensoltech@gmail.com>
2019-11-26 21:38:42 +0500

    updated code a bit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TypeORM-NodeJS-Assignment
-Very basic backend app with a single entity Customer (id, name, phone). I should be able to create a new customer, get list of customers, get customer by id and request by phone or name. Stack: Node, Express, TS, SQLite, TypeORM Just build very basic setup according my requirements, no need for super heavy logic. 
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-import 'reflect-metadata';
-import * as dotenv from 'dotenv';
-import { createConnection, Connection } from 'typeorm';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import {User} from "./entity/User";
 
-import * as server from './server';
+createConnection().then(async connection => {
 
-dotenv.config();
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
 
-async function run() {
-  try {
-    const connection: Connection = await createConnection();
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
 
-    const config: server.IConfig = {
-      port: process.env.PORT ? Number(process.env.PORT) : 8080,
-      deps: { connection },
-    };
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
 
-    server.start(config);
-  } catch (error) {
-    // tslint:disable-next-line
-    console.log(error);
-  }
-}
+    // setup express app here
+    // ...
 
-run();
+    // start express server
+    app.listen(3000);
+
+   
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+
+}).catch(error => console.log(error));
